881e1dc29bc4583bb14cbf509a880b0271155375
Monoxide5113 <monoxide5113@gmail.com>
2026-03-07 05:50:34 +0900

    feat: add 3085

---
+++ _3xxx/_30xx/3085(1).cpp
@@ -0,0 +1,82 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int get_max_candy(const std::vector<std::string>& board, int N, int row_start,
+                  int row_end, int col_start, int col_end)
+{
+    int ret{1};
+
+    for (int i{row_start}; i <= row_end; ++i) {
+        int row_cnt{1};
+
+        for (int j{0}; j < N - 1; ++j) {
+            if (board[i][j] == board[i][j + 1]) {
+                ++row_cnt;
+            }
+            else {
+                row_cnt = 1;
+            }
+
+            ret = std::max(ret, row_cnt);
+        }
+    }
+
+    for (int i{col_start}; i <= col_end; ++i) {
+        int col_cnt{1};
+
+        for (int j{0}; j < N - 1; ++j) {
+            if (board[j][i] == board[j + 1][i]) {
+                ++col_cnt;
+            }
+            else {
+                col_cnt = 1;
+            }
+
+            ret = std::max(ret, col_cnt);
+        }
+    }
+
+    return ret;
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int N{};
+    std::cin >> N;
+
+    std::vector<std::string> board(N);
+    for (auto& s : board) {
+        std::cin >> s;
+    }
+
+    int res{get_max_candy(board, N, 0, N - 1, 0, N - 1)};
+
+    for (int i{0}; i < N; ++i) {
+        for (int j{0}; j < N - 1; ++j) {
+            if (board[i][j] != board[i][j + 1]) {
+                std::swap(board[i][j], board[i][j + 1]);
+                res = std::max(res, get_max_candy(board, N, i, i, j, j + 1));
+                std::swap(board[i][j], board[i][j + 1]);
+            }
+        }
+    }
+
+    for (int i{0}; i < N; ++i) {
+        for (int j{0}; j < N - 1; ++j) {
+            if (board[j][i] != board[j + 1][i]) {
+                std::swap(board[j][i], board[j + 1][i]);
+                res = std::max(res, get_max_candy(board, N, j, j + 1, i, i));
+                std::swap(board[j][i], board[j + 1][i]);
+            }
+        }
+    }
+
+    std::cout << res << '\n';
+
+    return 0;
+}
