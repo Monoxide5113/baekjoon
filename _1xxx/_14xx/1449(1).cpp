b072a9322cf319f30d88936362a1db4551510ba8
Monoxide5113 <monoxide5113@gmail.com>
2026-03-09 03:01:56 +0900

    feat: add 1449

---
+++ _1xxx/_14xx/1449(1).cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int N{};
+    int L{};
+    std::cin >> N >> L;
+
+    std::vector<int> poses(N);
+    for (auto& p : poses) {
+        std::cin >> p;
+    }
+
+    std::sort(poses.begin(), poses.end());
+
+    int res{1};
+    int last_pos{poses[0]};
+    for (auto it{poses.cbegin() + 1}; it != poses.cend(); ++it) {
+        const auto& pos{*it};
+
+        if (pos - last_pos < L) {
+            continue;
+        }
+
+        last_pos = pos;
+        ++res;
+    }
+
+    std::cout << res << '\n';
+
+    return 0;
+}
