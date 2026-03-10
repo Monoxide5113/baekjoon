e9a51dfc0d032a88ea9efe6b8ff6c1015730de93
Monoxide5113 <monoxide5113@gmail.com>
2026-03-10 23:25:19 +0900

    feat: add 17509

---
+++ 17xxx/17509(1).cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <vector>
+
+constexpr int problem_cnt{11};
+
+constexpr int penalty{20};
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::vector<std::pair<int, int>> problems(problem_cnt);
+    for (auto& [D, V] : problems) {
+        std::cin >> D >> V;
+    }
+
+    std::sort(problems.begin(), problems.end());
+
+    int res{0};
+    int acc_time{0};
+    for (const auto& [D, V] : problems) {
+        acc_time += D;
+        res += acc_time + penalty * V;
+    }
+
+    std::cout << res << '\n';
+
+    return 0;
+}
