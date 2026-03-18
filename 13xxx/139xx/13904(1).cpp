86d446b9d62351a38e3b48d9a103f961974de2c7
Monoxide5113 <monoxide5113@gmail.com>
2026-03-18 23:30:10 +0900

    feat: add 13904

---
+++ 13xxx/139xx/13904(1).cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <bitset>
+#include <iostream>
+#include <utility>
+#include <vector>
+
+constexpr int max_day{1'001};
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int N{};
+    std::cin >> N;
+
+    std::vector<std::pair<int, int>> tasks(N);
+    for (auto& [d, w] : tasks)
+        std::cin >> d >> w;
+
+    std::sort(tasks.begin(), tasks.end(), [](auto a, auto b) {
+        if (a.second != b.second) {
+            return a.second > b.second;
+        }
+
+        return a.first > b.first;
+    });
+
+    int res{0};
+    std::bitset<max_day> days;
+    for (const auto [d, w] : tasks) {
+        for (int i{d}; i >= 1; --i) {
+            if (days[i]) {
+                continue;
+            }
+
+            days.set(i);
+            res += w;
+            break;
+        }
+    }
+
+    std::cout << res << '\n';
+
+    return 0;
+}
