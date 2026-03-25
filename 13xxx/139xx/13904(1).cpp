ea218470968d2e318ab9ebaf604c7ef172b9417d
Monoxide5113 <monoxide5113@gmail.com>
2026-03-25 23:12:37 +0900

    refactor: update 13904

--- 13xxx/139xx/13904(1).cpp
+++ 13xxx/139xx/13904(1).cpp
@@ -15,8 +15,9 @@ int main()
     std::cin >> N;
 
     std::vector<std::pair<int, int>> tasks(N);
-    for (auto& [d, w] : tasks)
+    for (auto& [d, w] : tasks) {
         std::cin >> d >> w;
+    }
 
     std::sort(tasks.begin(), tasks.end(), [](auto a, auto b) {
         if (a.second != b.second) {
@@ -28,7 +29,7 @@ int main()
 
     int res{0};
     std::bitset<max_day> days;
-    for (const auto [d, w] : tasks) {
+    for (const auto& [d, w] : tasks) {
         for (int i{d}; i >= 1; --i) {
             if (days[i]) {
                 continue;
